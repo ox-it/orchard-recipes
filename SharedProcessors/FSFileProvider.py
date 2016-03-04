7638542aadefc23b98f80ec40be43e8af0ac9bf7
Ben Goodstein <ben.goodstein@it.ox.ac.uk>
2016-03-04 10:31:56 +0000

    Changes to FSFileProvider.py to make it more useful when parsing versions

--- SharedProcessors/FSFileProvider.py
+++ SharedProcessors/FSFileProvider.py
@@ -20,75 +20,67 @@ from autopkglib import Processor, ProcessorError
 __all__ = ["FSFileProvider"]
 
 class FSFileProvider(Processor):
+    input_variables = {
+            're_pattern': {
+                'description': 'Regular expression (Python) to match against.',
+                'required': True,
 
+                },
+            'path': {
+                'description': 'provide the afs path',
+                'required': True,
+                },
+            'result_output_var_name': {
+                'description': 'desc',
+                'required': False,
+                'default': 'match',
+                },
+            }
 
-	input_variables = {
-		're_pattern': {
-				'description': 'Regular expression (Python) to match against.',
-				'required': True,
+    output_variables = {
+            'result_output_var_name': {
+                'description': 'desc',
+                },
+            }	
 
-			},
-		'path': {
-			      'description': 'provide the afs path',
-			      'required': True,
-			},
-		'result_output_var_name': {
-			      'description': 'desc',
-			      'required': False,
-                              'default': 'match',
-		        },
-	}
+    def get_path_and_search(self, path, re_pattern):
+        match_pattern = re.compile(re_pattern)
+        try:
+            software = os.listdir(path)
+        except OSError:
+            raise ProcessorError('Error Messages will get better with time'(path, re_pattern))
+        highestver = ""
+        finalmatch = None
+        for x in software:
+            match = re.match(match_pattern, x)
+            if match:
+                if highestver == "" or LooseVersion(x) > LooseVersion(highestver):
+                    highestver = x
+                    finalmatch = match
+        if finalmatch:
+            return (finalmatch.group(finalmatch.lastindex or 0), finalmatch.groupdict())
+        else:
+            raise ProcessorError('The regular expression \'%s\' does not match any file in path \'%s\'' % (re_pattern, path))
 
-	output_variables = {
-		'result_output_var_name': {
-			     'description': 'desc',
-		},
-	}	
+    def main(self):
+        output_var_name = self.env['result_output_var_name']
 
-	def get_path_and_search(self, path, re_pattern):
+        if 'path' in self.env:
+            path = self.env['path']
+        if 're_pattern' in self.env:
+            re_pattern = self.env['re_pattern']
 
+        groupmatch, groupdict = self.get_path_and_search(path,re_pattern)
 
-		try:
-			software = os.listdir(path)
-			highestver = software[0]
+        if output_var_name not in groupdict.keys():
+            groupdict[output_var_name] = groupmatch
 
-
-			for x in range(len(software)):
-					if re.search(re_pattern,software[x]) and LooseVersion(software[x]) > LooseVersion(highestver):
-						highestver = software[x]
-		except OSError:
-			raise ProcessorError('Error Messages will get better with time'(path, re_pattern))
-
-		try:
-			re_pattern = re.compile(re_pattern)
-			match = re_pattern.search(highestver)
-
-			return (match.group(match.lastindex or 0), match.groupdict())
-		except:
-			pass
-
-
-	def main(self):
-
-		output_var_name = self.env['result_output_var_name']
-		
-
-		if 'path' in self.env:
-			path = self.env['path']
-		if 're_pattern' in self.env:
-			re_pattern = self.env['re_pattern']
-
-		groupmatch, groupdict = self.get_path_and_search(path,re_pattern)
-		
-	        if output_var_name not in groupdict.keys():
-			groupdict[output_var_name] = groupmatch
-
-		self.output_variables = {}
-		for key in groupdict.keys():
-			self.env[key] = groupdict[key]
-			self.output('Found matching text (%s): %s' % (key, self.env[key], ))
-			self.output_variables[key] = {
-				'description': 'Matched regular expression group'}
+        self.output_variables = {}
+        for key in groupdict.keys():
+            self.env[key] = groupdict[key]
+            self.output('Found matching text (%s): %s' % (key, self.env[key], ))
+            self.output_variables[key] = {
+                    'description': 'Matched regular expression group'}
 
 if __name__ == '__main__':
-	PROCESSOR = FSFileProvider()
+    PROCESSOR = FSFileProvider()
